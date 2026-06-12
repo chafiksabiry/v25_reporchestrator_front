a2f62582cd120b838f72ebcedb4446e78db9d826
elhoucineqara114 <elhoucine.qara@harx.ai>
2026-06-12 18:19:28 +0100

    Update

--- src/components/dashboard/CallRecords.tsx
+++ src/components/dashboard/CallRecords.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 import { useTranslation } from 'react-i18next';
 import {
